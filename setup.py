a6b4dd6d09857341cbef5efb33a5d5897f381ed3
Hugo Vaysset <hugo.vaysset@curie.fr>
2021-06-15 15:03:00 +0200

    added __init__ file everywhere

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="deep_learning_models",
+    name="deep_learning",
     version="0.0.1",
     author="Hugo Vaysset",
     author_email="hugo.vaysset@polytechnique.edu",
-    description="Diverse deep learning models for general purposes",
+    description="Diverse deep learning tools and models for general purposes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hugovaysset/deep_learning_models",
+    url="https://github.com/hugovaysset/deep_learning",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3',
-    install_requires=['numpy', 'scipy', 'tensorflow', 'keras_preprocessing']
+    install_requires=[]
 )
