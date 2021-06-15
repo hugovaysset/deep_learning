5d4912a707a83cdc646c96c9e6fd05ce25a4bc2d
Hugo Vaysset <hugo.vaysset@curie.fr>
2021-06-15 16:32:38 +0200

    added plot learning curves callback

--- deep_learning/callbacks/diagnostic.py
+++ deep_learning/callbacks/diagnostic.py
@@ -5,6 +5,8 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 
+plt.style.use('ggplot')
+
 class PredictOnImagesCallback(tf.keras.callbacks.Callback):
     """
     Callback to make segmentation predictions on test images in the end of each epoch.
@@ -29,4 +31,42 @@ class PredictOnImagesCallback(tf.keras.callbacks.Callback):
             ax.imshow(im.squeeze(-1), cmap="gray")
             ax.imshow(pred.squeeze(-1), cmap="Blues", alpha=0.4)
             plt.axis('off')
-            plt.savefig(f"{self.save_dir}/epoch_{epoch}_{im_path}", bbox_inches="tight", format="png")
+            plt.savefig(f"{self.save_dir}/epoch_{epoch}_{im_path}.png", bbox_inches="tight", format="png")
+
+
+class SaveLearningCurvesCallbakc(tf.keras.callbacks.Callback):
+    """
+    Callback to plot and save the learning curves loss=f(epochs) and metrics=f(epochs) from epoch==0
+    to epoch==current epoch. Allows to make a movie of the learning curves over time.
+    """
+
+    def __init__(self, save_dir, metrics=None):
+        """
+        save_dir (str): path to save the curves
+        metrics (str or tf.keras.metrics): metrics to plot
+        """
+        self.save_dir = save_dir
+        self.metrics = metrics
+
+    def on_epoch_end(self, epoch, logs={}):
+        fig, ax = plt.subplots(1, 2, figsize=(20, 8))
+ 
+        # plot learning curves
+        ax[0].plot(logs["loss"][:], "orange", label="loss")
+        ax[0].plot(logs["val_loss"][:], "b", label="validation loss")
+        ax[0].legend()
+        ax[0].set_title("Training curves")
+        ax[0].set_xlabel("Epochs")
+        ax[0].set_ylabel(f"Loss")
+        # ax[0].set_ylabel("Loss (MSE)")
+
+        ax[1].plot(logs[f"{self.metrics}"][:], "orange", label=f"{self.metrics}")
+        ax[1].plot(logs[f"val_{self.metrics}"][:], "b", label=f"validation {self.metrics}")
+        ax[1].legend()
+        ax[1].set_title("Training curves")
+        ax[1].set_xlabel("Epochs")
+        ax[1].set_ylabel("IoU")
+
+        fig.suptitle(f"Epoch {epoch}")
+
+        plt.savefig(f"{self.save_dir}/epoch_{epoch}_learning_curve.png", format="png")
